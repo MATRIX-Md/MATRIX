fd943ed5fc93c3c44e97bb09f69ae4b938fa190d
MATRIX-BOT-MD <159259011+MATRIX-BOT-MD@users.noreply.github.com>
2024-04-25 10:22:19 +0100

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -16,7 +16,7 @@ ${result.lyrics}
 
     } catch (error) {
         console.error('Error:', error);
-        conn.reply(m.chat, `وقعت مشكلة راسل نورالدين \ninstagram.com/noureddine_ouafy`, m);
+        conn.reply(m.chat, `وقعت مشكلة راسل عبدالإله \ninstagram.com/matrix__tv5`, m);
     }
 }
 
