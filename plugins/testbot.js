5dc7c6671b8e11e58f17242fe821f808ff86e349
MATRIX-BOT-MD <159259011+MATRIX-BOT-MD@users.noreply.github.com>
2024-04-25 11:33:07 +0100

    Update testbot.js

--- plugins/testbot.js
+++ plugins/testbot.js
@@ -1,13 +1,10 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `بوبيزة متصلة الان بالانترنيت \nيمكنك كتابة \n.menu\n لتنبتق لك جميع الاوامر \n@${m.sender.split('@')[0]} \n`
+let info = `ماتريكس متصلة الان بالانترنيت \nيمكنك كتابة \n.menu\n لتنبتق لك جميع الاوامر \n@${m.sender.split('@')[0]} \n`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 65,
       isForwarded: true, externalAdReply: { title: author, body: bottime,thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
       await conn.sendMessage(m.chat, {
-        audio: {
-            url: "bobizaramadhan.mp3"
-        },
         seconds: 65,
         ptt: true,
         mimetype: "audio/mpeg",
@@ -17,7 +14,7 @@ await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forw
         quoted: m
     })
 }
-handler.customPrefix = /^(tes|سلام|menu|Menu|apk|bobizaty|♥|salam|hy|Hello|.|شكرا|مرحبا)$/i
+handler.customPrefix = /^(tes|سلام|menu|Menu|apk|matrix|♥|salam|hy|Hello|•|شكرا|مرحبا)$/i
 handler.command = new RegExp
 
 export default handler
